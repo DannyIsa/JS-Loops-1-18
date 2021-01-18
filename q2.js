f272f07bbd9ee38bdedb9e14d6a7db1b82e06ebe
DannyIsa <DannyIsa@users.noreply.github.com>
2021-01-18 14:08:49 +0200

    q2

---
+++ q2.js
@@ -0,0 +1,8 @@
+let firstNumber = Number (prompt ('insert first number'));
+let secondNumber = Number (prompt ('insert second number'));
+let thirdNumber = Number (prompt ('insert third number'));
+if ((firstNumber + secondNumber + thirdNumber) > 0){
+    alert ('+');
+}else {
+    alert ('-');
+}
