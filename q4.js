e469a44eda6dc76954b7c14909e2ce484f4eef9d
DannyIsa <DannyIsa@users.noreply.github.com>
2021-01-18 14:57:04 +0200

    q4

---
+++ q4.js
@@ -0,0 +1,21 @@
+let a = 1;
+let b = 10;
+let c = -5;
+let d = 1.5;
+let e = Infinity;
+if (a>b && a>c && a>d && a>e)
+{
+    alert(a);
+}else if (b>a && b>c && b>d && b>e)
+{
+    alert(b);
+}else if (c>a && c>b && c>d && c>e)
+{
+    alert(c);
+}else if (d>a && d>c && d>b && d>e)
+{
+    alert(d);
+}else
+{
+    alert(e);
+}
