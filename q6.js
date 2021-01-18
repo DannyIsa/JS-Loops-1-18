228df52473e12859454bb3de7d9856a0ea2be148
DannyIsa <DannyIsa@users.noreply.github.com>
2021-01-18 15:39:13 +0200

    q6

---
+++ q6.js
@@ -0,0 +1,22 @@
+let david = 80;
+let vinoth = 77;
+let divya = 88;
+let ishitha = 95;
+let thomas = 68;
+let avg = (david + vinoth + divya + ishitha + thomas) / 5;
+console.log(avg);
+if (avg < 60){
+    console.log('F');
+     
+}else if (avg < 70){
+    console.log('D');
+     
+}else if (avg < 80){
+    console.log('C');
+     
+}else if (avg < 90){
+    console.log('B');
+     
+}else if (avg < 100){
+    console.log('A');
+}
