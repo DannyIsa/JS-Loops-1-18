d705e19df1cb314631fc35f1fbcf26608686988b
DannyIsa <DannyIsa@users.noreply.github.com>
2021-01-18 13:55:22 +0200

    q1

---
+++ q1.js
@@ -0,0 +1,7 @@
+let firstNumber = Number (prompt ('insert first number'));
+let secondNumber = Number (prompt ('insert second number'));
+if (firstNumber>secondNumber){
+    alert(firstNumber);
+}else{
+    alert(secondNumber);
+}
