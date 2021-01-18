fa75ffe73d569e0517c19ee0b14463608c192f36
DannyIsa <DannyIsa@users.noreply.github.com>
2021-01-18 15:12:29 +0200

    q5

---
+++ q5.js
@@ -0,0 +1,14 @@
+let i = 0;
+while (i<=15){
+    if (i === 0){
+        console.log(i + ' is even');
+        i++
+    }
+    else if (i % 2 === 0){
+        console.log(i + ' is even');
+        i++
+    }else{
+        console.log(i + ' is odd');
+        i++
+    }
+}
